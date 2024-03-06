c8532e4510100c4e1c4d3a49dca9a6fa551ee0a0
Ihsan Lutfi <94753442+milkucoklatpremium@users.noreply.github.com>
2024-03-06 17:20:26 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// trigger to play music in the background with sweetalert
+window.addEventListener('load', () => {
+    Swal.fire({
+        title: 'Apakah anda ingin memutar musik?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.querySelector('.song').play();
+            animationTimeline();
+        } else {
+            animationTimeline();
+        }
+    });
+});
