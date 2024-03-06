ab8b61c75845c6a263a30072e3d3f5dd02dfae2b
Ihsan Lutfi <94753442+milkucoklatpremium@users.noreply.github.com>
2024-03-06 17:21:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Apakah anda ingin memutar musik?',
+        title: 'Apakah ingin memutar musik?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
